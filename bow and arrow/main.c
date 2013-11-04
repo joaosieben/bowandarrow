83d4500483e7bfec39521188692d7d71fe1d5dc2
Joao Afonso Peres Sieben <japsieben@s-72-206-07.(none)>
2013-11-04 11:33:19 -0200

    criação da função movearcher_down, e adaptação para sistema linux.

--- bow and arrow/main.c
+++ bow and arrow/main.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
-#include <conio.h>
-#include <Windows.h>
+#include <stdlib.h>
+//#include <conio.h>
+//#include <Windows.h>
 #include <string.h>
+#include <termios.h> // função 'getch' para linux
+
+// Adaptação da função 'getch' para linux.
+static struct termios old, new;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo)
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  new = old; /* make new settings same as old settings */
+  new.c_lflag &= ~ICANON; /* disable buffered i/o */
+  new.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &new); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void)
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo)
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void)
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void)
+{
+  return getch_(1);
+}
+// ------------------------------
 
 void gotoxy (int x, int y); // função para posicionar o cursor na tela, na posição desejada.
 int menu (void); // função para inicializar o menu principal do jogo.
@@ -12,8 +56,6 @@ void print_borders (void); // função para imprimir as bordas do mapa atual.
 // funções de movimento.
 void movearcher_up (void);
 void movearcher_down (void);
-void movearcher_right (void);
-void movearcher_left (void);
 
 // variáveis globais.
 int Garcher_x, Garcher_y;
@@ -26,10 +68,11 @@ int main () {
 }
 
 void gotoxy (int x, int y) {
-	COORD coord = {0, 0};
-	coord.X = x;
-	coord.Y = y;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+//	COORD coord = {0, 0};
+//	coord.X = x;
+//	coord.Y = y;
+//	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    printf("%c[%d;%df",0x1B,y,x);
 }
 
 int menu (void) {
@@ -86,7 +129,7 @@ void newgame (void) {
 
 void level1 (void) {
     int score, arrows, balloons=15, cont1, cont2, archer_x, archer_y;
-    char map[24][80], archer[4][8], arrow[] = "-->", balloon[2][2], key;
+    char map[24][80], archer[3][8], arrow[] = "-->", balloon[2][2], key;
     // construindo o arqueiro.
     for (cont1=0; cont1<3; cont1++) {
         for (cont2=0; cont2<8; cont2++) {
@@ -122,21 +165,15 @@ void level1 (void) {
     key = getch();
     do {
         switch (key) {
-        case 72: // seta para cima.
+        case 119: // tecla 'w', que move o arqueiro para cima.
             movearcher_up();
             break;
-        case 80: // seta para baixo.
+        case 115: // tecla 's', que move o arqueiro para baixo.
             movearcher_down();
-            break;
-        case 77: // seta para a direita.
-            movearcher_right();
-            break;
-        case 75: // seta para a esquerda.
-            movearcher_left();
             break;
         }
         key = getch();
-        printf("%i", key);
+       // printf("%i", key);
     } while (key != 27);
 }
 
@@ -167,14 +204,16 @@ void print_borders (void) {
 
 void movearcher_up (void) {
     int cont1, cont2, archer_x, archer_y;
-    char archer[4][8];
-    if (Garcher_y-1 > 3) {
+    char archer[3][8];
+    if (Garcher_y-1 > 4) {
         strcpy(archer[0], "~O |\\   ");
         strcpy(archer[1], " |<| >->");
         strcpy(archer[2], " | |/   ");
         strcpy(archer[3], "/ \\     ");
-        system("cls");
-        print_borders();
+        for (cont1=0; cont1<8; cont1++) {
+            gotoxy((Garcher_x+cont1), (Garcher_y+3));
+            printf(" ");
+        }
         Garcher_y--;
         archer_x = Garcher_x;
         archer_y = Garcher_y;
@@ -188,11 +227,26 @@ void movearcher_up (void) {
     }
 }
 void movearcher_down (void) {
-
-}
-void movearcher_right (void){
-
-}
-void movearcher_left (void) {
-
+    int cont1, cont2, archer_x, archer_y;
+    char archer[3][8];
+    if (Garcher_y+1 < 21) {
+        strcpy(archer[0], "~O |\\   ");
+        strcpy(archer[1], " |<| >->");
+        strcpy(archer[2], " | |/   ");
+        strcpy(archer[3], "/ \\     ");
+        for (cont1=0; cont1<8; cont1++) {
+            gotoxy((Garcher_x+cont1), Garcher_y);
+            printf(" ");
+        }
+        Garcher_y++;
+        archer_x = Garcher_x;
+        archer_y = Garcher_y;
+        for (cont1=0; cont1<4; cont1++) {
+            gotoxy(archer_x, archer_y);
+            for (cont2=0; cont2<8; cont2++) {
+                printf("%c", archer[cont1][cont2]);
+            }
+            archer_y++;
+        }
+    }
 }
